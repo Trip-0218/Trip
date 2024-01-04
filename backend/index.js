e6b88b2caa5dbd0d49e1373a19657260b9eb297b
Abhange Vishal <106510224+vishalabhange@users.noreply.github.com>
2024-01-04 14:11:18 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const connectToMongo = require('./db');
+const express = require('express')
+var cors = require("cors")
+
+connectToMongo();
+
+
+const app = express()
+app.use(cors())
+
+const port = 8080
+
+app.use(express.json())
+
+//available routes
+app.use('/api/auth', require("./routes/auth"))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
